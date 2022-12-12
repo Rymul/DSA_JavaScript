aacdf514ef69d621734f75824dbb27d2436738fd
Ryan Mullen <ryanfmullen@gmail.com>
2022-12-12 13:40:14 -0800

    complete group anagrams

--- algo-academy/hash-sets.js
+++ algo-academy/hash-sets.js
@@ -41,3 +41,40 @@ console.log([[0,0,0,0],[0,4,5,0],[0,3,1,0]], "Expected Output" )
 
 
 
+/**
+ * Group Anagrams
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+ var groupAnagrams = function(strs) {
+    const anagrams = {};
+
+    for(let word of strs) {
+        const key = letterCount(word);
+        if(!anagrams[key]) anagrams[key] = [];
+        anagrams[key].push(word);
+    }
+
+    return Object.values(anagrams);
+};
+
+const letterCount = function(word){
+    const key = new Array(26).fill(0);
+
+    for(let i = 0; i < word.length; i++){
+        const charCode = word.charCodeAt(i) - "a".charCodeAt(0);
+        key[charCode]++;
+    }
+    return key;
+}
+
+
+let s = ["eat","tea","tan","ate","nat","bat"]
+console.log(groupAnagrams(s))
+console.log([["bat"],["nat","tan"],["ate","eat","tea"]], "Expected Output" )
+
