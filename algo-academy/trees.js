aaceb7d2092f1e6482c138e52be3e96703e97813
Ryan Mullen <ryanfmullen@gmail.com>
2022-12-13 15:46:09 -0800

    complete inorder binary tree traversal

--- algo-academy/trees.js
+++ algo-academy/trees.js
@@ -43,3 +43,37 @@ console.log([], "Expected Output")
 
 
 
+/**
+ * 94. Binary Tree Inorder Traversal
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    // if(!root) return [];
+    // let left = inorderTraversal(root.left);
+    // let right = inorderTraversal(root.right);
+    // let middle = root.val;
+    // return [...left, middle, ...right];
+    
+    if(!root) return [];
+    let left = inorderTraversal(root.left);
+    left.push(root.val)
+    let right = inorderTraversal(root.right);
+    return left.concat(right);
+};
+
+let x = [1,null,2,3]
+console.log(inorderTraversal(x))
+console.log([1,3,2], "expected output")
