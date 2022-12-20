e3488721f50c575c2bba47137b67df527ef15f83
Ryan Mullen <ryanfmullen@gmail.com>
2022-12-19 21:29:34 -0800

    complete search insert position

--- arrays.js
+++ arrays.js
@@ -38,3 +38,35 @@ console.log(3, "expected output")
 let numbers2 = [2,2,1,1,1,2,2]
 console.log(majorityElement(numbers2))
 console.log(2, "expected output")
+
+
+
+
+/**
+ * 35. Search Insert Position
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    if(target < nums[0]) return 0
+    for(i=0; i < nums.length; i++){
+        if(nums[i] === target) {
+            return i
+        } else if(nums[i] < target && target < nums[i+1]) {
+            return i+1
+        } else if (i === nums.length-1) {
+            return nums.length
+        }
+    }
+};
+
+let nums1 = [1,3,5,6]
+let target1 = 5
+console.log(searchInsert(nums1, target1))
+console.log(2, "expected output")
