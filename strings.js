c4540e66e2a44066527026a23c15ed030ff992cd
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-05 22:10:46 -0800

    complete reverse string

--- strings.js
+++ strings.js
@@ -85,3 +85,25 @@
     return res.join("")
   
 };
+
+
+
+
+
+/**
+ * 344. Reverse String
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ */
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+  for (let i = 0; i < s.length/2; i++) {
+      let temp = s[i]
+      s[i] = s[s.length-i-1]
+      s[s.length-i-1] = temp
+  }
+};
