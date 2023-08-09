f7eb03a92fb865a270b5be09ed13ec68e1bc064f
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-08 21:57:57 -0700

    complete longest common prefix

--- leetcode.js
+++ leetcode.js
@@ -120,3 +120,25 @@ var singleNumber = function(nums) {
 };
 
 console.log(singleNumber([4,1,2,1,2]))
+
+// 14. Longest Common Prefix
+
+var longestCommonPrefix = function(strs) {
+    let res = "";
+    if (strs.length === 0) return res;
+    
+    for(let i = 0; i < strs[0].length; i++){
+        const char = strs[0][i];
+        
+        for(let j = 1; j < strs.length; j++) {
+            if(char !== strs[j][i]) {
+                return res;
+            }
+        }
+        res = res + char;
+    }
+    return res;
+};
+
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
