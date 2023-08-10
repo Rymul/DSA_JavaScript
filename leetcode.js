4fc70b21e35eb5ae3c4259b75f61829792e96b50
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-09 18:42:23 -0700

    complete merge two sorted lists

--- leetcode.js
+++ leetcode.js
@@ -168,3 +168,23 @@ var isValid = function(s) {
 
 console.log(isValid("()[]{}"))
 console.log(isValid("(}"))
+
+// 21. Merge Two Sorted Lists
+
+var mergeTwoLists = function(list1, list2) {
+    let current = new ListNode(-1);
+    let tempHead = current;
+    
+    while(list1 || list2) {
+        if(list1 === null || (list2 !== null && list1.val >= list2.val)){
+            current.next = list2
+            current = current.next
+            list2 = list2.next
+        } else {
+            current.next = list1
+            current = current.next
+            list1 = list1.next
+        }    
+    }
+    return tempHead.next
+};
