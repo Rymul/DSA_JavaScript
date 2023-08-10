1b9096ebc772563327b6f762b5524bdcbf382d1f
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-09 18:44:04 -0700

    complete remove duplicates from sorted array

--- leetcode.js
+++ leetcode.js
@@ -188,3 +188,19 @@ var mergeTwoLists = function(list1, list2) {
     }
     return tempHead.next
 };
+
+// 26. Remove Duplicates from Sorted Array
+
+var removeDuplicates = function(nums) {
+    let i = 0;
+    while (i < nums.length) {
+        if(nums[i] === nums[i+1]){
+            nums.splice(i, 1);
+        } else {
+            i++
+        }
+    }
+    return nums.length
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
