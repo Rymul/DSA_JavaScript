2e1e1e3ddb13c7ff8974037e858c17b83a1eaf7a
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-15 19:14:14 -0700

    complete is isomorphic

--- leetcode.js
+++ leetcode.js
@@ -293,3 +293,26 @@ var hasCycle = function(head) {
     }
     return false;
 };
+
+
+// 205. Isomorphic Strings
+
+var isIsomorphic = function(s, t) {
+    
+    if(s.length !== t.length) return false;
+    let countS = {};
+    let countT = {};
+    let res = true
+    
+    for( i = 0; i < s.length; i++) {
+        if(!countS[ s[i] ] && !countT[ t[i] ]){
+            countS[ s[i] ] = t[i];
+            countT[ t[i] ] = s[i];
+        } else if ( countS[ s[i] ] !== t[i] || countT[ t[i] ] !== s[i]) {
+            res = false;
+        }
+    }
+    return res;
+}
+
+console.log(isIsomorphic('egg', 'add'))
