645c8a717349b54a0d12ab2d754c64c6b9eef25c
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-16 20:17:05 -0700

    complete reverse string

--- leetcode.js
+++ leetcode.js
@@ -364,3 +364,17 @@ var invertTree = function(root) {
     root.right = left;
     return root;
 };
+
+
+// 344. Reverse String
+
+var reverseString = function(s) {
+    for (let i = 0; i < s.length/2; i++) {
+        let temp = s[i]
+        s[i] = s[s.length-i-1]
+        s[s.length-i-1] = temp
+    }
+    return s
+};
+
+console.log(reverseString(["h","e","l","l","o"]))
