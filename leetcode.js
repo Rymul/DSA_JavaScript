9afe5a3b00375d01c27a4ba9d100f2baacd52050
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-17 20:17:19 -0700

    complete majority element

--- leetcode.js
+++ leetcode.js
@@ -378,3 +378,30 @@ var reverseString = function(s) {
 };
 
 console.log(reverseString(["h","e","l","l","o"]))
+
+
+// 169. Majority Element
+
+var majorityElement = function(nums) {
+    nums.sort()
+    let res = []
+    let count = 1
+    let max = 0
+    
+    if(nums.length === 1) return nums[0]
+    for( let i = 0; i < nums.length; i++){
+        if(nums[i] === nums[i+1]){
+            count++
+        } else {
+            count=1
+        }
+        if(max < count){
+            res.push(nums[i])
+            max = count
+        }
+    } 
+    return res[res.length-1]
+
+};
+
+console.log(majorityElement([1,1,2,2,2,3]))
