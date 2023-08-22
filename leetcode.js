492921790c1a5cd3dddddc4ae89cbde9c2588684
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-21 20:27:47 -0700

    complete find the difference

--- leetcode.js
+++ leetcode.js
@@ -498,3 +498,18 @@ var addBinary = function(a, b) {
 
   return answer.join("");
 };
+
+
+// 389. Find the Difference
+
+var findTheDifference = function(s, t) {
+   s = s.split("").sort();
+   t = t.split("").sort();
+   
+   for(let i = 0; i < s.length; i++){
+       if( s[i] !== t[i]) {
+           return t[i]
+       }
+   }
+   return t[t.length -1] 
+};
