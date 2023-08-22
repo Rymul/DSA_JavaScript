369d36afdd8684629b51ff3761c84ed066cfa5a6
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-21 20:26:23 -0700

    complete add binary

--- leetcode.js
+++ leetcode.js
@@ -473,3 +473,28 @@ var fizzBuzz = function(n) {
 };
 
 console.log(fizzBuzz(5))
+
+
+// 67. Add Binary
+
+var addBinary = function(a, b) {
+    const arrA = a.split("").reverse();
+    const arrB = b.split("").reverse();
+    let carry = 0;
+    let answer = [];
+    let i = 0;
+    const len = a.length > b.length ? a.length : b.length;
+
+  while (i < len) {
+    const x = arrA[i] ? +arrA[i] : 0;
+    const y = arrB[i] ? +arrB[i] : 0;
+    const sum = carry + x + y;
+    carry = Math.floor(sum / 2);
+    answer.unshift(sum % 2);
+    i++;
+  }
+
+  if (carry > 0) answer.unshift(carry);
+
+  return answer.join("");
+};
