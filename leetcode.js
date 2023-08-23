52ba6acf072268eecba79209c3357721dde4af2d
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-22 20:53:17 -0700

    complete add two numbers

--- leetcode.js
+++ leetcode.js
@@ -530,3 +530,28 @@ var findTheDifference = function(s, t) {
   }
   return numOfOdds > 0 ? s.length - numOfOdds + 1 : s.length;
 };
+
+
+// 2. Add Two Numbers
+
+var addTwoNumbers = function(l1, l2) {
+    let res = new ListNode(0);
+    let current = res;
+    let carry = 0
+
+    while(l1 || l2 || carry){
+        let sum = carry;
+        if(l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if(l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next
+    }
+    return res.next;
+};
