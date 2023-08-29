0afd26993624f47262f14e3471e49b7ad5832d31
Ryan Mullen <ryanfmullen@gmail.com>
2023-08-28 20:41:52 -0700

    637. Average of Levels in Binary Tree

--- leetcode.js
+++ leetcode.js
@@ -640,3 +640,23 @@ var findWords = function(words) {
 
 console.log(findWords(["Hello","Alaska","Dad","Peace"]))
 
+
+// 637. Average of Levels in Binary Tree
+
+var averageOfLevels = function(root) {
+    const sum = [];
+const count = []
+const traverse = (node, i) => {
+    if(sum[i] === undefined) sum[i] = 0;
+    if(count[i] === undefined) count[i] = 0;
+    sum[i] += node.val;
+    count[i]++;
+    if(node.left) traverse(node.left, i + 1);
+    if(node.right) traverse(node.right, i + 1)
+}
+traverse(root, 0)
+for(let i = 0; i < sum.length; i++){
+    sum[i] = sum[i] / count[i]
+}
+return sum;
+};
