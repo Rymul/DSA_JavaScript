b8911264687e266dca19074f712506d030d17719
Ryan Mullen <ryanfmullen@gmail.com>
2023-09-04 19:55:05 -0700

    complete 121. Best Time to Buy and Sell Stock

--- leetcode.js
+++ leetcode.js
@@ -743,3 +743,25 @@ var longestPalindrome = function(s) {
 
 console.log(longestPalindrome('babad'))
 console.log(longestPalindrome('cbbd'))
+
+
+// 121. Best Time to Buy and Sell Stock
+
+var maxProfit = function(prices) {
+    let left = 0;
+    let right = 1;
+    let max_profit = 0;
+    while (right < prices.length) {
+        if (prices[left] < prices[right]) {
+            let profit = prices[right] - prices[left];
+            max_profit = Math.max(max_profit, profit);
+        } else {
+            left = right;
+        }
+        right ++;
+    }
+    return max_profit;
+};
+
+console.log(maxProfit([7,1,5,3,6,4]))
+console.log(maxProfit([7,6,4,3,1]))
