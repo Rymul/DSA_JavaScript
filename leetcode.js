f815966c0629396cf3a3f3726a5a57c243d1ecf5
Ryan Mullen <ryanfmullen@gmail.com>
2023-09-05 17:41:10 -0700

    complete 605. Can Place Flowers

--- leetcode.js
+++ leetcode.js
@@ -795,3 +795,19 @@ var isAnagram = function(s, t) {
 
 console.log(isAnagram("anagram", "nagaram"))
 console.log(isAnagram("dog", "cat"))
+
+
+// 605. Can Place Flowers
+
+var canPlaceFlowers = function(flowerbed, n) {
+    for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+        if (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+            n--;
+            i++;
+        }
+    }
+    return n === 0
+};
+
+console.log(canPlaceFlowers([1,0,0,0,1], 1))
+console.log(canPlaceFlowers([1,0,0,0,1], 2))
