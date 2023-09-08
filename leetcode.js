78ac42f2b4fe88928ff49342c67f41df003df3c6
Ryan Mullen <ryanfmullen@gmail.com>
2023-09-07 21:04:14 -0700

    complete 867. Transpose Matrix

--- leetcode.js
+++ leetcode.js
@@ -865,3 +865,21 @@ var lemonadeChange = function(bills) {
 
 console.log(lemonadeChange([5,5,5,10,20]))
 console.log(lemonadeChange([5,5,10,10,20]))
+
+
+// 867. Transpose Matrix
+
+var transpose = function(matrix) {
+    let res = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        let col = [];
+        for (let j = 0; j < matrix.length; j++) {
+            col.push(matrix[j][i]);
+        }
+        res.push(col);
+    }
+    return res;
+};
+
+console.log(transpose([[1,2,3],[4,5,6],[7,8,9]]))
+console.log(transpose([[1,4],[2,5],[3,6]]))
