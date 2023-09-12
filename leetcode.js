68dce5ecbab059d13e522ea81123c8f86306ccd6
Ryan Mullen <ryanfmullen@gmail.com>
2023-09-11 21:10:44 -0700

    complete 495. Teemo Attacking

--- leetcode.js
+++ leetcode.js
@@ -917,3 +917,19 @@ var surfaceArea = function(grid) {
 
 console.log(surfaceArea([[1,2],[3,4]]))
 console.log(surfaceArea([[1,1,1],[1,0,1],[1,1,1]]))
+
+
+// 495. Teemo Attacking
+
+
+var findPoisonedDuration = function(timeSeries, duration) {
+    if (timeSeries.length === 0) return 0;
+    let res = duration;
+    for (let i = 1; i < timeSeries.length; i++) {
+        res += Math.min(timeSeries[i] - timeSeries[i - 1], duration)
+    }
+    return res;
+};
+
+console.log(findPoisonedDuration([1,4], 2))
+console.log(findPoisonedDuration([1,2], 2))
