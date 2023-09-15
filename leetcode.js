cf8da70aeaa5aba8a77ba9f83c55f0b3729c7ef8
Ryan Mullen <ryanfmullen@gmail.com>
2023-09-15 12:28:30 -0700

    complete 1089. Duplicate Zeros

--- leetcode.js
+++ leetcode.js
@@ -1035,3 +1035,20 @@ var heightChecker = function(heights) {
 
 console.log(heightChecker([1,1,4,2,1,3]))
 console.log(heightChecker([1,2,3,4,5]))
+
+
+// 1089. Duplicate Zeros
+
+var duplicateZeros = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0){
+            arr.splice(i,0,0);
+            arr.pop();
+            i += 1;
+        }
+    }
+    return arr;
+};
+
+console.log(duplicateZeros([1,0,2,3,0,4,5,0]))
+console.log(duplicateZeros([1,2,3]))
