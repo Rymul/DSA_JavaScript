9e2931d13af0a38847f40026028456ab37134683
Ryan Mullen <ryanfmullen@gmail.com>
2023-09-26 17:42:14 -0700

    complete 572. Subtree of Another Tree

--- leetcode.js
+++ leetcode.js
@@ -1237,3 +1237,17 @@ const validPalindromeHelper = (s, low, high) => {
 
 console.log(validPalindrome('abca'))
 console.log(validPalindrome('abc'))
+
+
+// 572. Subtree of Another Tree
+
+var isSubtree = function(root, subRoot) {
+    if (!root) return !subRoot;
+    return isSame(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
+const isSame = (root1, root2) => {
+    if (!root1 || !root2) return !root1 && !root2;
+    if (root1.val !== root2.val) return false;
+    return isSame(root1.left, root2.left) && isSame(root1.right, root2.right);
+}
