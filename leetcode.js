a377e2323d9033657a7818742b0c646b9be104b3
Ryan Mullen <ryanfmullen@gmail.com>
2023-10-12 18:02:33 -0700

    complete 643. Maximum Average Subarray I

--- leetcode.js
+++ leetcode.js
@@ -1471,3 +1471,22 @@ var rotateString = function(s, goal) {
 console.log(rotateString("abcde", "cdeab"))
 console.log(rotateString("abcde", "abced"))
 
+
+// 643. Maximum Average Subarray I
+
+var findMaxAverage = function(nums, k) {
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    let maxSum = sum;
+    for (let i = k; i < nums.length; i++) {
+        sum = sum - nums[i - k] + nums[i];
+        maxSum = Math.max(maxSum, sum); 
+    }
+    return maxSum / k;
+};
+
+console.log(findMaxAverage([1,12,-5,-6,50,3], 4))
+console.log(findMaxAverage([5], 1))
+
