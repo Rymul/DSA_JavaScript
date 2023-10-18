8c338fae51db0ad74bb5085a0da1e5f7b1b85a2a
Ryan Mullen <ryanfmullen@gmail.com>
2023-10-17 19:01:25 -0700

    complete 263. Ugly Number

--- leetcode.js
+++ leetcode.js
@@ -1518,3 +1518,26 @@ const memo = {
 
 console.log(tribonacci(4))
 console.log(tribonacci(25))
+
+
+// 263. Ugly Number
+
+var isUgly = function(n) {
+    if (n <= 0) return false;
+    while (n != 1) {
+        if (n % 2 === 0) {
+            n /= 2
+        } else if (n % 3 === 0) {
+            n /= 3
+        } else if (n % 5 === 0) {
+            n /= 5
+        } else {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isUgly(6))
+console.log(isUgly(1))
+console.log(isUgly(14))
