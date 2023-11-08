e1a566e8f28e5cdb3358f2c3b5f8a6f86ffa4292
Ryan Mullen <ryanfmullen@gmail.com>
2023-11-07 16:56:21 -0800

    complete 15. 3Sum

--- leetcode.js
+++ leetcode.js
@@ -1849,3 +1849,39 @@ var numberOfLines = function(widths, s) {
 
 console.log(numberOfLines([10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "abcdefghijklmnopqrstuvwxyz"))
 console.log(numberOfLines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "bbbcccdddaaa"))
+
+
+
+// 15. 3Sum
+
+var threeSum = function(nums) {
+    const res = [];
+    if (nums.length < 3) return res;
+    nums = nums.sort((a, b) => a - b);
+    let target = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] > target) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum === target) {
+                res.push([nums[i], nums[j], nums[k]]);
+                while(nums[j] === nums[j + 1]) j++;
+                while(nums[k] === nums[k - 1]) k--;
+                j++;
+                k--;
+            } else if (sum < target) {
+                j++;
+            } else {
+                k--;
+            }
+
+        }
+    }
+    return res;
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([0,1,1]))
