a39d291e80439b5dd94e0c308d30ee407ee5c863
Ryan Mullen <ryanfmullen@gmail.com>
2023-11-08 14:51:22 -0800

    complete 859. Buddy Strings

--- leetcode.js
+++ leetcode.js
@@ -1927,3 +1927,24 @@ var backspaceCompare = function(s, t) {
 
 console.log(backspaceCompare("ab##", "c#d#"))
 console.log(backspaceCompare("a#c", "b"))
+
+
+
+// 859. Buddy Strings
+
+var buddyStrings = function(s, goal) {
+    if (s.length !== goal.length) return false;
+    if (s === goal) {
+        const set = new Set(s);
+        return set.size < s.length
+    }
+    const diff = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== goal[i]) diff.push(i);
+        if (diff.length > 2) return false;
+    }
+    return diff.length === 2 && s[diff[0]] === goal[diff[1]] && s[diff[1]] === goal[diff[0]];
+};
+
+console.log(buddyStrings('ab', 'ba'))
+console.log(buddyStrings('ab', 'ab'))
