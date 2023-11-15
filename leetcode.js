02769d77772da1ba45f49ed96e7d4af7bda8f96d
Ryan Mullen <ryanfmullen@gmail.com>
2023-11-14 16:28:54 -0800

    complete 17. Letter Combinations of a Phone Number

--- leetcode.js
+++ leetcode.js
@@ -2004,3 +2004,28 @@ var isToeplitzMatrix = function(matrix) {
 
 console.log(isToeplitzMatrix([[1,2,3,4],[5,1,2,3],[9,5,1,2]]))
 console.log(isToeplitzMatrix([[1,2],[2,2]]))
+
+
+
+// 17. Letter Combinations of a Phone Number
+
+var letterCombinations = function(digits) {
+    const phoneDigits = {'2':"abc",'3':"def",'4':"ghi",'5':"jkl",'6':"mno",'7':"pqrs",'8':"tuv",'9':"wxyz"} 
+    let res = [];
+    if (!digits) return res;
+    const dfs = (pos, str) => {
+        if (pos === digits.length) {
+            res.push(str);
+        } else {
+            let letters = phoneDigits[digits[pos]];
+            for (let i = 0; i < letters.length; i++) {
+                dfs(pos + 1, str + letters[i])
+            }
+        }
+    }
+    dfs(0, "")
+    return res;
+};
+
+console.log(letterCombinations("23"))
+console.log(letterCombinations("2"))
