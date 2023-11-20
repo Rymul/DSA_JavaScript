c714614bc77d285d0deacd685cb8313613e01925
Ryan Mullen <ryanfmullen@gmail.com>
2023-11-20 13:13:51 -0800

    complete 19. Remove Nth Node From End of List

--- leetcode.js
+++ leetcode.js
@@ -2029,3 +2029,22 @@ var letterCombinations = function(digits) {
 
 console.log(letterCombinations("23"))
 console.log(letterCombinations("2"))
+
+
+
+// 19. Remove Nth Node From End of List
+
+var removeNthFromEnd = function(head, n) {
+    let fast = head;
+    let slow = head;
+    for (i = 0; i < n; i++) {
+        fast = fast.next
+    }
+    if (!fast) return head.next;
+    while (fast.next) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+};
