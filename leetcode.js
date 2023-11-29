692cd06c4239c541a1a2efd2a0b0f04209e7c47b
Ryan Mullen <ryanfmullen@gmail.com>
2023-11-29 14:40:16 -0800

    complete 33. Search in Rotated Sorted Array

--- leetcode.js
+++ leetcode.js
@@ -2175,3 +2175,36 @@ var maxCoins = function(piles) {
 
 console.log(maxCoins([2,4,1,2,7,8]))
 console.log(maxCoins([2,4,5]))
+
+
+
+// 33. Search in Rotated Sorted Array
+
+var searchRotatedSorted = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if (nums[middle] === target) {
+            return middle;
+        }
+        if (nums[left] <= nums[middle]) {
+            if (nums[left] <= target && target <= nums[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        } else {
+            if (nums[middle] <= target && target <= nums[right]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+    }
+    return -1;
+};
+
+console.log(searchRotatedSorted([4,5,6,7,0,1,2], 0))
+console.log(searchRotatedSorted([4,5,6,7,0,1,2], 3))
