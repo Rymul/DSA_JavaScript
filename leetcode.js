6fc043e94973ce92de2284a1863116fd9751b45d
Ryan Mullen <ryanfmullen@gmail.com>
2024-01-04 18:45:19 -0800

    complete 48. Rotate Image

--- leetcode.js
+++ leetcode.js
@@ -2434,3 +2434,25 @@ var maxSubArray = function(nums) {
 
 console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 console.log(maxSubArray([5,4,-1,7,8]))
+
+
+
+// 48. Rotate Image
+
+var rotate = function(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i + 1; j < matrix[i].length; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] =  matrix[j][i];
+            matrix[j][i] = temp;
+
+        }
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i].reverse();
+    }
+    return matrix;
+};
+
+console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]))
+console.log(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
