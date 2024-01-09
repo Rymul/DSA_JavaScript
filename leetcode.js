c977a2aeec6bb88f5da753935a066d8f0469aadd
Ryan Mullen <ryanfmullen@gmail.com>
2024-01-08 18:34:11 -0800

    complete 64. Minimum Path Sum

--- leetcode.js
+++ leetcode.js
@@ -2471,3 +2471,28 @@ var uniquePaths = function(m, n) {
 
 console.log(uniquePaths(3, 7))
 console.log(uniquePaths(3, 2))
+
+
+
+// 64. Minimum Path Sum
+
+var minPathSum = function(grid) {
+    const col = grid.length;
+    const row = grid[0].length;
+
+    for (let i = 1; i < col; i++) {
+        grid[i][0] += grid[i - 1][0];
+    }
+    for (let j = 1; j < row; j++) {
+        grid[0][j] += grid[0][j - 1];
+    }
+    for (let i = 1; i < col; i++) {
+        for (let j = 1; j < row; j++) {
+            grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+        }
+    }
+    return grid[col - 1][row - 1];
+};
+
+console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+console.log(minPathSum([[1,2,3],[4,5,6]]))
