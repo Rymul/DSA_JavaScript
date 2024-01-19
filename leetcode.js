4f78bfd009de91ed8e3780d5d66a4efae2665ced
Ryan Mullen <ryanfmullen@gmail.com>
2024-01-19 15:18:05 -0800

    complete 74. Search a 2D Matrix

--- leetcode.js
+++ leetcode.js
@@ -2672,3 +2672,23 @@ var maxProfit2 = function(prices) {
 
 console.log(maxProfit2([7,1,5,3,6,4]))
 console.log(maxProfit2([7,6,4,3,1]))
+
+
+
+// 74. Search a 2D Matrix
+
+var searchMatrix = function(matrix, target) {
+    if (!matrix.length || !matrix[0].length) return false;
+    let row = 0;
+    let col = matrix[0].length - 1;
+
+    while (col >= 0 && row <= matrix.length - 1) {
+        if (matrix[row][col] === target) return true;
+        else if (matrix[row][col] > target) col--;
+        else if (matrix[row][col] < target) row++;
+    }
+    return false;
+};
+
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
