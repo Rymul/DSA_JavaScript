db3097aa7aa90469170925ec596d13bbf3564ba3
Ryan Mullen <ryanfmullen@gmail.com>
2024-01-24 17:05:31 -0800

    complete 27. Remove Element

--- leetcode.js
+++ leetcode.js
@@ -2790,3 +2790,21 @@ var isPalindrome = function(x) {
 
 console.log(isPalindrome(121))
 console.log(isPalindrome(10))
+
+
+// 27. Remove Element
+
+var removeElement = function(nums, val) {
+    let i = 0
+    while(i < nums.length){
+        if(nums[i] === val) {
+            nums.splice(i, 1)
+        } else {
+            i++
+        }
+    }
+    return nums.length
+};
+
+console.log(removeElement([3,2,2,3], 3))
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
