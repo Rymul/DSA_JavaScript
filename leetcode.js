872bb0d0601c26d96cbf9a8f940b73e9d4b507e2
Ryan Mullen <ryanfmullen@gmail.com>
2024-01-31 18:25:02 -0800

    complete 13. Roman to Integer

--- leetcode.js
+++ leetcode.js
@@ -2949,3 +2949,26 @@ console.log(numIslands([
     ["0","0","0","1","1"]
   ]))
 
+
+
+// 13. Roman to Integer
+
+var romanToInt = function(s) {
+    let numerals = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        numerals[s[i]] < numerals[s[i + 1]] ? res -= numerals[s[i]] : res += numerals[s[i]]
+    }
+    return res;
+};
+
+console.log(romanToInt("III"))
+console.log(romanToInt("LVIII"))
