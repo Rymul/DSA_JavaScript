8b09385fa99ba008443500d3f0fa6ecf0211f58a
Ryan Mullen <ryanfmullen@gmail.com>
2024-02-01 17:13:43 -0800

    complete 120. Triangle

--- leetcode.js
+++ leetcode.js
@@ -2972,3 +2972,19 @@ var romanToInt = function(s) {
 
 console.log(romanToInt("III"))
 console.log(romanToInt("LVIII"))
+
+
+
+// 120. Triangle
+
+var minimumTotal = function(triangle) {
+    for (let i = triangle.length - 2; i >= 0; i--) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            triangle[i][j] += Math.min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        }
+    }
+    return triangle[0][0];
+};
+
+console.log(minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]]))
+console.log(minimumTotal([[-10]]))
