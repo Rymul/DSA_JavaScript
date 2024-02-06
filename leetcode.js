3fbf329fe9e5db167b5d6c115882c8ccb9cc554c
Ryan Mullen <ryanfmullen@gmail.com>
2024-02-05 18:27:11 -0800

    complete 86. Partition List

--- leetcode.js
+++ leetcode.js
@@ -2988,3 +2988,29 @@ var minimumTotal = function(triangle) {
 
 console.log(minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]]))
 console.log(minimumTotal([[-10]]))
+
+
+
+// 86. Partition List
+
+var partition = function(head, x) {
+    let before = new ListNode(0);
+    let after = new ListNode(0);
+    let beforeCurr = before;
+    let afterCurr = after;
+
+    while (head) {
+        if (head.val < x) {
+            beforeCurr.next = head;
+            beforeCurr = beforeCurr.next;
+        } else {
+            afterCurr.next = head;
+            afterCurr = afterCurr.next;
+        }
+        head = head.next;
+    }
+    afterCurr.next = null;
+    beforeCurr.next = after.next
+    return before.next;
+};
+
