f0b6efd4b882ee9305f12a7f4b6eaf2ead231b24
Ryan Mullen <ryanfmullen@gmail.com>
2024-02-07 20:24:28 -0800

    complete 16. 3Sum Closest

--- leetcode.js
+++ leetcode.js
@@ -3031,3 +3031,31 @@ var createCounter = function(n) {
 async function sleep(millis) {
     await new Promise(resolve => setTimeout(resolve, millis));
 }
+
+
+
+// 16. 3Sum Closest
+
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    let closest = Infinity;
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+            if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                closest = sum;
+            }
+            if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+    return closest;
+};
+
+console.log(threeSumClosest([-1,2,1,-4], 1))
+console.log(threeSumClosest([0,0,0], 1))
