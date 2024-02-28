27c2e5dbb5a0e17e333036344a8d5934091e0adf
Ryan Mullen <ryanfmullen@gmail.com>
2024-02-27 19:05:13 -0800

    complete 162. Find Peak Element

--- leetcode.js
+++ leetcode.js
@@ -3309,3 +3309,25 @@ var repeatedNTimes = function(nums) {
 console.log(repeatedNTimes([1,2,3,3]))
 console.log(repeatedNTimes([2,1,2,5,3,2]))
 console.log(repeatedNTimes([5,1,5,2,5,3,5,4]))
+
+
+// 162. Find Peak Element
+
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+        let mid = Math.floor((right + left) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+};
+
+
+console.log(findPeakElement([1,2,3,1]))
+console.log(findPeakElement([1,2,1,3,5,6,4]))
