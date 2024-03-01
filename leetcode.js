ba5f4b2713a35e7199aa405f974e5681a517f95b
Ryan Mullen <ryanfmullen@gmail.com>
2024-02-29 18:09:26 -0800

    complete 167. Two Sum II - Input Array Is Sorted

--- leetcode.js
+++ leetcode.js
@@ -3350,3 +3350,28 @@ var maximumGap = function(nums) {
 
 console.log(maximumGap([3,6,9,1]))
 console.log(maximumGap([10]))
+
+
+
+// 167. Two Sum II - Input Array Is Sorted
+
+var twoSum2 = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return[-1, -1];
+};
+
+console.log(twoSum2([2,7,11,15], 9))
+console.log(twoSum2([2,3,4], 6))
+console.log(twoSum2([-1,0], -1))
