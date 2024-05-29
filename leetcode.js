f9fe1bf52dc199e7e0a0e8c5b657ce42c70c819a
Ryan Mullen <ryanfmullen@gmail.com>
2024-05-28 20:34:18 -0700

    complete 2724. Sort By

--- leetcode.js
+++ leetcode.js
@@ -4108,3 +4108,15 @@ var addedInteger = function(nums1, nums2) {
 console.log(addedInteger([2,6,4], [9,7,5]))
 console.log(addedInteger([10], [5]))
 console.log(addedInteger([1,1,1,1], [1,1,1,1]))
+
+
+// 2724. Sort By
+
+var sortBy = function(arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b))
+};
+
+console.log(sortBy([5, 4, 1, 2, 3], fn = (x) => x))
+console.log(sortBy([{"x": 1}, {"x": 0}, {"x": -1}], fn = (d) => d.x))
+console.log(sortBy([[3, 4], [5, 2], [10, 1]], fn = (x) => x[1]))
+
